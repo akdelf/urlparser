79f0900560383a1a9f70feffea42dc95bc32a315
AK Delfin <ak@argumenti.ru>
2014-11-14 15:11:27 +0300

    parser $_SERVER parametrs

--- urlparse.php
+++ urlparse.php
@@ -1,21 +1,34 @@
 <?php 
 
-		function url_parse($url, $separ = '/') {
+		function url_parser($uri = null) {
 
-			$items = explode($separ, urldecode($url));
+			
+			if ($uri == null and isset($_SERVER['QUERY_STRING'])){ // default = QUERY_STRING
+				$uri = $_SERVER['QUERY_STRING'];
+				$result['protocol'] = $_SERVER['REQUEST_SCHEME'];
+				$result['domain'] = $_SERVER['HTTP_HOST'];
+				$result['method'] =  $_SERVER['REQUEST_METHOD'];
+				$items = explode($separ, $uri);
+			}	
+			else {
+			
+				if ($find = strpos($uri, '://')) { // http, https
+					$result['protocol'] = substr($uri, 0, $find);
+					$uri = substr($uri, $find + 3);
+				}
 
-			print_r($items);
+				$items = explode('/', $uri);
 
-			$control = array_shift($items);
-			$action = array_shift($items);
-			$params = $items; // more params
+				if (filter_var(gethostbyname($items[0]), FILTER_VALIDATE_IP)) //domain name
+					$result['domain'] = array_shift($items);
 
-			return array (
-				
-				'control' => $control,
-				'action' => $action,
-				'params' => $params
-				
-			);
+			}		
+
+			$result['control'] = array_shift($items);
+			$result['action'] = array_shift($items);
+			$result['params'] = $items; // more params
+
+			
+			return $result;
 
 		}
